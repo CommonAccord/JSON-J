9105adf22bdd46d007545b54eae422b09c1f279a
ya <ya@yas-macbook-pro-2.local>
2014-08-21 10:19:00 -0300

    CMACC directory contains the new (old-fashioned) CMACC parser

---
+++ CMACC/parser.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl -wl
+
+use warnings;
+use strict;
+
+my @missing;
+my $path = "./repository/";
+
+sub parse {
+	
+	my($file,$root) = @_; my $f;
+
+	ref($file) eq "GLOB" ? $f = $file : open $f, $file or die $!;
+	
+	my $content = parse_root($f, $root);
+
+	if($content) { expand_fields($f, \$content); return($content) }
+
+	return;
+}
+
+
+sub parse_root { 
+	
+	my ($f, $field) = @_; my $root;
+
+	seek($f, 0, 0);	
+	while(<$f>) {
+		return $root if ($root) = $_ =~ /^$field\s*=\s*(.*?)$/;
+	}
+	
+	seek($f, 0, 0);
+	while(<$f>) {
+		my($part,$what);	
+		if( (($part, $what) = $_ =~ /^([^=]*)=\[(.+?)\]/) and ($field =~ s/^$part//) ) {
+			$root = parse($path.$what, $field);
+		}
+	}
+	return $root;
+
+} 
+
+sub expand_fields  {
+
+	my($f,$field) = @_;
+
+	foreach( $$field =~ /\{([^}]+)\}/g ) {
+		my $ex = $_;
+		my $value = parse($f, $ex);
+		$value ? $$field =~ s/\{$ex\}/$value/gg : push @missing, $ex;
+	}
+} 
+
+
+
+print "\nResult:\n".parse($ARGV[0], "Model.Root");
+print "\nMissing elements: @missing";
+
