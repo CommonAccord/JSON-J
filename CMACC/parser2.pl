3ed8d6c0709720aa63bf6a95b40fb513d3c97c12
James Hazard <james.g.hazard@gmail.com>
2014-08-24 17:42:40 -0400

    500 Startups - KISS - Demo 2 -- works!

--- CMACC/parser2.pl
+++ CMACC/parser2.pl
@@ -57,5 +57,5 @@ sub expand_fields  {
 
 
 
-print "\nResult:\n".parse($ARGV[0], "Model.Root");
-print "\nMissing elements: @missing";
+print " ".parse($ARGV[0], "Model.Root");
+# print "\nMissing elements: @missing";
